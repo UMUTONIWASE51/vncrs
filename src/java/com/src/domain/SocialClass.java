8f56ad7d0ac13ea3d391aca0a94e56927c4d0c31
daphnet <daphnethirwa51@gmail>
2021-04-08 08:49:36 -0700

    implementation of User and Belonging class

--- src/java/com/src/domain/SocialClass.java
+++ src/java/com/src/domain/SocialClass.java
@@ -19,7 +19,7 @@ import javax.persistence.SequenceGenerator;
  */
 @Entity
 public class SocialClass implements Serializable {
-
+private static  final long serialVersionUID = 1L;
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     @SequenceGenerator(name = "auto_gen", sequenceName = "A")
