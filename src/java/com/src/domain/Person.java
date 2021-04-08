42e315a379e3b1643d5c4b6713da77f776b142cc
daphnet <daphnethirwa51@gmail>
2021-04-08 08:49:09 -0700

    implementation of User and Belonging class

--- src/java/com/src/domain/Person.java
+++ src/java/com/src/domain/Person.java
@@ -17,7 +17,7 @@ import javax.persistence.Id;
  */
 @Entity
 public class Person implements Serializable{
-
+private static  final long serialVersionUID = 1L;
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer personId;
