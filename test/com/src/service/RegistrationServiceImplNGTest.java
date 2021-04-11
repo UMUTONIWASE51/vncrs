af797cdb54eacb5fb747bf2b70d5938245e13a62
daphnet <daphnethirwa51@gmail>
2021-04-10 19:17:39 -0700

    Added Registration test case for Create and Save to DB

--- test/com/src/service/RegistrationServiceImplNGTest.java
+++ test/com/src/service/RegistrationServiceImplNGTest.java
@@ -6,6 +6,7 @@
 package com.src.service;
 import com.src.dao.GeneralDao;
 import com.src.domain.Person;
+import com.src.domain.Registration;
 import com.src.domain.SocialClass;
 import java.util.Date;
 import org.testng.Assert;
@@ -19,8 +20,9 @@ import org.testng.annotations.Test;
 public class RegistrationServiceImplNGTest {
 
     private String nid;
-    private Person person;
+    private Person person, umwana, umukozi;
     private SocialClass socialStatus;
+    private Registration registration;
     private RegistrationService service;
 
     public RegistrationServiceImplNGTest() {
@@ -28,27 +30,46 @@ public class RegistrationServiceImplNGTest {
 
     @BeforeMethod
     public void setUpMethod() throws Exception {
-        nid = "11998000097";
-        person = new Person("Umutoniwase", "Daphnet", new Date(), "Female", "1199865656565656", "larsonbitch@gmail.com", "07887777777");
-        socialStatus = new SocialClass("Upper");
         service = new RegistrationServiceImpl();
+        nid = "11998000097";
+        //How to create a Person Object using Constructor
+        person = new Person("Umutoniwase", "Daphnet", new Date(), "Female", "1199865656565656", "larsonbitch@gmail.com", "07887777777");
+        umwana = new Person("Umwana", "Muto", new Date(), "Male", "", "larsonbitch@gmail.com", "07887777777");
+        umukozi = new Person("Umukozi", "Karyarugo", new Date(), "Female", "1199865656565656", "larsonbitch@gmail.com", "07887777777");
+        //How to create a SociaClass object using Constructor
+        socialStatus = new SocialClass("Upper");
+        //Saving the socialClass object before using it for Registration
+        service.createSocialClass(socialStatus);
+        //Saving the Person ojbect before using it for Registration
+        service.createNewPerson(person);
+        //How to create a Regsistration object using Constructor
+        registration = new Registration("Akagari", new Date(), person, socialStatus);
+        //Adding a child as a member of the family
+        registration.addMember(umwana);
+        //Adding a housgirl as a member of the family
+        registration.addMember(umukozi);
     }
 
     @Test
     public void testGenerateSocialClass() {
-        service.createSocialClass(socialStatus);
         SocialClass social = service.getSocialClassById((Integer)1);
         Assert.assertEquals(social.getDescription(), "Upper");
     }
 
     @Test
-    public void testRegisterNewPerson() {
-        service.createNewPerson(person);
+    public void testCreateNewPerson() {
         Assert.assertEquals(service.getPersonById(1).getNid(), "1199865656565656");
     }
 
     @Test
-    public void testGetPerson() {
+    public void testRegisterNewComer() {
+        //We only savea this Registration if and only if we already saved the Person and SocialClass objects!
+        service.createRegistration(registration);
+        Assert.assertNotNull(registration);
+        Assert.assertEquals(registration.getFamilyChief().getFirstName(), "Umutoniwase");
+        Assert.assertEquals(registration.getMembers().size(), 2);
+        Assert.assertEquals(registration.getSocialClass().getDescription(), "Upper");
+        Assert.assertEquals(registration.getBelongings().size(), 0);
     }
 
     @Test
